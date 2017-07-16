0c602060ccc7ab4af00623b1e154844609e51dff
John Xia <john.danger.xia@gmail.com>
2017-07-16 11:51:53 -0400

    Hair physics tweaks, UI improvements
    * hair now cuts on mousemove
    * there is a floor
    * added damping & progressive stiffness/density changes

--- hairmit.js
+++ hairmit.js
@@ -36,7 +36,6 @@ Runner.run(runner, engine);
 
 var GROUP = Body.nextGroup(true);
 
-
 function manyHairs(follicles) {
   var hairs = Composite.create();
   follicles.forEach(({start, angle}) => {
@@ -50,31 +49,49 @@ world.gravity.scale = 0.01;
 
 function oneHair(start, hairOpts) {
   var opts = {
-    length: 50,
+    length: 30,
     width: 15,
     angle: Math.PI,
-    segmentsPerHair: 10,
-    angularStiffness: 0.9,
-    stiffness: 0.7
+    segmentsPerHair: 20,
+    angularStiffness: 0.95,
+    stiffness: 0.6,
+    kinkiness: 0.0,
+    overlap: 0.4,
   };
   Object.assign(opts, hairOpts);
   // make actual hair bodies
-  var strand = Composites.stack(start.x, start.y, opts.segmentsPerHair, 1, -20, 0, function(x, y) {
-    return Bodies.rectangle(x, y, opts.length, opts.width, {
+  var strand = Composites.stack(
+    start.x,
+    start.y,
+    opts.segmentsPerHair,
+    1,
+    -opts.length * opts.overlap,
+    0, (x, y) =>
+      Bodies.rectangle(x, y, opts.length, opts.width, {
       collisionFilter: { group: GROUP },
-      frictionAir: 0,
+      frictionAir: 0.1,
       chamfer: 5,
       render: {
         fillStyle: 'transparent',
         lineWidth: 1,
-        strokeStyle: '#41485b',
+        strokeStyle: '#41485b'
       }
-    });
-  });
+    })
+  );
+
+  strand.bodies.forEach(
+    (body, i) => Body.setDensity(body, body.density * Math.pow(0.95, i))
+  );
+  strand
+    .constraints
+    .filter(({label}) => label === "pin")
+    .forEach((pin, i) => pin.angularStiffness = Math.pow(0.98, i)) ;
   // attach segments to each other
-  Composites.chain(strand, 0.45, 0, -0.45, 0, {
-    stiffness: 0.99,
+  Composites.chain(strand, 0.4, 0, -0.4, 0, {
+    label: "pin",
     length: 0,
+    stiffness: 1,
+    damping: 0.5,
     angularStiffness: opts.angularStiffness,
     render: {
       strokeStyle: '#4a485b'
@@ -82,8 +99,11 @@ function oneHair(start, hairOpts) {
   });
   // make them tense
   Composites.chain(strand, 0, 0, 0, 0, {
+    label: "tension",
+    length: opts.length * (1 - opts.overlap/2) * (1 - opts.kinkiness),
     stiffness: opts.stiffness,
-    length: opts.length,
+    damping: 0.5,
+    anchor: false,
     render: {
       strokeStyle: '#4a485b'
     }
@@ -140,6 +160,9 @@ var follicles = [
   {start: {x: 200, y: 200}, angle: Math.PI*0.9},
 ];
 
+var ground = Bodies.rectangle(350, 590, 800, 20, { isStatic: true });
+World.add(world, ground);
+
 var hairs = manyHairs(follicles);
 World.add(world, hairs);
 
@@ -160,7 +183,7 @@ var mouseConstraint = MouseConstraint.create(engine, {
 
 function makeCuttable(hair) {
   Events.on(
-    mouseConstraint, "mousedown",
+    mouseConstraint, "mousemove",
     ({source: {body}}) => {
       if (body && hair.bodies.includes(body)) {
         World.remove(hair, [body], true);
